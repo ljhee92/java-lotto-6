6850a143d6ef3c32b9dc17552ad5e88f9e1f832b
ljhee92 <weilainib7@gmail.com>
2024-10-10 17:07:45 +0900

    feat: 보너스번호 객체 생성

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+public class BonusNumber {
+    private final int bonusNumber;
+    private final WinningNumber winningNumber;
+
+    public BonusNumber(int bonusNumber, WinningNumber winningNumber) {
+        validate(bonusNumber, winningNumber);
+        this.bonusNumber = bonusNumber;
+        this.winningNumber = winningNumber;
+    } // BonusNumber
+
+    private void validate(int bonusNumber, WinningNumber winningNumber) {
+        checkBetweenOneAndFourtyFive(bonusNumber);
+        checkDupilicate(bonusNumber, winningNumber);
+    } // validate
+
+    private void checkBetweenOneAndFourtyFive(int bonusNumber) {
+        if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        } // end if
+    } // checkBetweenOneAndFourtyFive
+
+    private void checkDupilicate(int bonusNumber, WinningNumber winningNumber) {
+        if (winningNumber.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호와 중복되지 않는 숫자여야 합니다.");
+        } // end if
+    } // checkDuplicate
+} // class
