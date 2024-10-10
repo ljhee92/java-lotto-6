94a5aa9e7a5e26b1b40e549c000d1d8af9d336dc
ljhee92 <weilainib7@gmail.com>
2024-10-10 17:05:51 +0900

    feat: view 생성

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        sort(numbers);
+        this.numbers = numbers;
+    } // Lotto
+
+    private void validate(List<Integer> numbers) {
+        checkNumberSize(numbers);
+        checkBetweenOneAndFourtyFive(numbers);
+        checkDupilicate(numbers);
+    } // validate
+
+    private void checkNumberSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자 6개여야 합니다.");
+        } // end if
+    } // checkNumberSize
+
+    private void checkBetweenOneAndFourtyFive(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(number -> number >= 1 && number <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        } // end if
+    } // checkBetweenOneAndFourtyFive
+
+    private void checkDupilicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
+        } // end if
+    } // checkDuplicate
+
+    private void sort(List<Integer> numbers) {
+        numbers.sort(Integer::compareTo);
+    } // sort
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    } // toString
+} // class
