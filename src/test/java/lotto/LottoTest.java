a3c9744eccc5c467e260169d2c751ce2bd9bda5c
ljhee92 <weilainib7@gmail.com>
2024-10-11 16:35:36 +0900

    test: 로또 생성 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.domain.LottoMachine;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +27,34 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호가 1~45 사이가 아니라면 예외가 발생한다.")
+    void createExceptionLottoByOneToFourtyFive() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 1~45 사이의 숫자로 생성되는지 확인한다.")
+    void createLottoByOneToFourtyFive() {
+        assertThat(new LottoMachine().createLotto()).allMatch(number -> number >= 1 && number <= 45);
+    }
+
+    @Test
+    @DisplayName("로또 번호의 개수가 6개인지 확인한다.")
+    void createLottoBySixCount() {
+        assertThat(new LottoMachine().createLotto()).hasSize(6);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 중복되지 않는지 확인한다.")
+    void createLottoByNotDuplicate() {
+        assertThat(new LottoMachine().createLotto()).doesNotHaveDuplicates();
+    }
+
+    @Test
+    @DisplayName("사용자가 입력한 구입 개수만큼 로또가 생성되는지 확인")
+    void createLottoByUserInputQuantity() {
+        assertThat(new LottoMachine().createLottos(3)).hasSize(3);
+    }
 }
