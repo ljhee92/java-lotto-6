dc5ce4f8273e17fde01a32d966d860d79c05c1c8
ljhee92 <weilainib7@gmail.com>
2024-10-14 16:23:43 +0900

    style: 공백 라인 삭제

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -10,5 +10,4 @@ public class ErrorMessage {
     public static final String BONUS_MUST_BE_BETWEEN_ONE_AND_FOURTYFIVE = PREFIX + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String BONUS_MUST_NOT_BE_DUPLICATE_OF_LOTTO = PREFIX + "보너스 번호는 로또 번호와 중복되지 않는 숫자여야 합니다.";
     public static final String AMOUNT_MUST_BE_DIVIDE_INTO_1000 = PREFIX + "구입금액은 1,000원 단위로 입력해야 합니다.";
-
 } // class
